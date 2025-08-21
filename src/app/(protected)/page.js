deb237b932dc39cac15617e3e7dbf8bebf928ad4
ovidas <designerovi00@gmail.com>
2025-08-21 15:28:50 +0600

    fix: short exam datetime

--- src/app/(protected)/page.js
+++ src/app/(protected)/page.js
@@ -96,8 +96,6 @@ const Index = () => {
                         day: "2-digit",
                         month: "short",
                         year: "numeric",
-                        hour: "2-digit",
-                        minute: "2-digit",
                       })
                     : "--"}
                 </p>
