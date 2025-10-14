20f61a3f09af6928852e9f4f3ee98de5c14144b9
ovidas00 <designerovi00@gmail.com>
2025-10-14 11:05:53 +0600

    feat: update editor

--- src/app/(protected)/ide/page.jsx
+++ src/app/(protected)/ide/page.jsx
@@ -21,18 +21,23 @@ export default function HtmlEditorDemo() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 md:p-6">
       {/* Editor */}
-      <div className="flex-1 flex-shrink-0 border border-gray-300">
+      <div className="flex-1 flex-shrink-0 border border-gray-300 dark:border-gray-600">
         <CodeMirror
           value={code}
           height="500px"
           extensions={[html()]}
           onChange={(value) => setCode(value)}
+          theme={localStorage.getItem("theme") === "dark" ? "dark" : "light"}
         />
       </div>
 
       {/* Live Preview */}
-      <div className="flex-1 border flex-shrink-0 border-gray-300 bg-white">
-        <iframe title="Live Preview" className="w-full h-full" srcDoc={code} />
+      <div className="flex-1 border flex-shrink-0 border-gray-300 dark:border-gray-600 bg-white">
+        <iframe
+          title="Live Preview"
+          className="w-full h-[500px]"
+          srcDoc={code}
+        />
       </div>
     </div>
   );
